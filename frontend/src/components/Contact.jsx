6ae262cdaff468976a8953542088716cba7bfbb4
matthew <matthewyt765@gmail.com>
2024-10-21 22:04:12 +0800

    adding a background color in contact (continuesTom

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,8 +1,12 @@
+import '../styles/sections/_contacts.scss';
+
 function Contact() {
     return (
-        <>
-        
-        </>    
+        <section id='contacts'>
+            <div style={{height: 2000}}>
+
+            </div>
+        </section>
     )
 }
 
