d102ac97ac75672e9f46758ac5efa443270ffb5d
matthew <matthewyt765@gmail.com>
2024-10-21 21:53:51 +0800

    Fcking erorrs. FINISHED the about page

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -16,30 +16,33 @@ function About() {
                 <h1 className='text-center text-primary display--1 about-title'>About</h1>
                 <p>
                     &nbsp;&nbsp;&nbsp;&nbsp; At Hot Potato Muntinlupa, our fries are more than just crispy they’re crave-worthy! With 
-                    flavors like classic <span className='text-primary'>cheese</span>, bold <span className='text-secondary'>barbecue</span>, tangy <span className='text-sourc'>sour & cream</span>, and our top-selling <span className='text-sweetc'>sweet corn</span>,
+                    flavors like classic <b><span className='text-primary'>Cheese</span></b>, bold <b><span className='text-secondary'>Barbecue</span></b>, tangy <b><span className='text-sourc'>Sour & Cream</span></b>, and our top-selling <b><span className='text-sweetc'>Sweet Corn</span></b>,
                     each bite is packed with flavor. Don’t forget to pair them with our indulgent yet balanced 
                     cheese sticks. Whether for a quick snack or a satisfying treat, Hot Potato has the perfect 
                     combo for you!
                 </p>
 
                 <Container>
-                    <Row>
-                        <Col lg={4}>
+                    <Row className='gap-4'>
+                        <Col className='about-col'>
+                            <p className='display--2 text-center'>About us </p>
+                            <div className='about-hr'> </div>
                             <div className='about-container'>
-                                <p className='display--2 text-center'>About us </p>
-                                <div className=''>
-                                    <img src="https://placehold.co/200" className='mx-auto d-block'  alt="picture of hot potato muntinlupa" />
+                                
+                                <div>
+                                    <img src="https://placehold.co/200" className='mx-auto d-block about-img'  alt="picture of hot potato muntinlupa" />
                                 </div>
                                 <div className='text-justify about-paragraph'>
                                     <p>Hot Potato in Muntinlupa, opened on July 22, 2024, is owned by Sheryl Anne Cabance and managed by Lheslie C. Fernandez. Located at Cap's Foodpark near SMS Montessori, it is known for its crispy fries and the tagline "Not your ordinary fries."</p>
                                 </div>
                             </div>
                         </Col>
-                        <Col lg={4}>
+                        <Col className='about-col'>
+                            <p className='display--2 text-center'>Mission </p>
+                            <div className='about-hr'> </div>
                             <div className='about-container'>
-                                <p className='display--2 text-center'>Mission </p>
-                                <div className=''>
-                                    <img src="https://placehold.co/200" className='mx-auto d-block'  alt="picture of mission" />
+                                <div>
+                                    <img src="https://placehold.co/200" className='mx-auto d-block about-img'  alt="picture of mission" />
                                 </div>
                                 <div className='text-justify about-paragraph'>
                                     <p>
@@ -50,11 +53,13 @@ function About() {
                                 </div>
                             </div>
                         </Col>
-                        <Col lg={4}>
+                        <Col className='about-col'>
+                            <p className='display--2 text-center'>Vision </p>
+                            <div className='about-hr'> </div>
                             <div className='about-container'>
-                                <p className='display--2 text-center'>Vision </p>
-                                <div className=''>
-                                    <img src="https://placehold.co/200" className='mx-auto d-block'  alt="picture of mission" />
+                                
+                                <div>
+                                    <img src="https://placehold.co/200" className='mx-auto d-block about-img'  alt="picture of mission" />
                                 </div>
                                 <div className='text-justify about-paragraph'>
                                     <p>The values of Hot Potato are centered on "Quality First," ensuring freshly cooked fries made from top-quality ingredients, and "Customer Satisfaction," prioritizing a delightful and satisfying experience for every customer.</p>
