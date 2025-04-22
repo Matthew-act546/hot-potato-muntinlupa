5740c142b95b1b209f2f0f1b0f40b803bd5ed907
matthew <matthewyt765@gmail.com>
2025-04-22 21:53:30 +0800

    fix color of the facebook icon and fix apostrapodfhy

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -7,7 +7,6 @@ import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
 function Contact() {
     return (
         <section id='contacts'>
-            
             <Container>
             <h1 className='display--1 text-white text-center'>Contact</h1>
                 <Row>
@@ -21,24 +20,23 @@ function Contact() {
                                 </span> 
                             </div>
                             <div>
-                                <span className='contact-icons'>
+                                <span className='contact-icons text-black'>
                                     <FontAwesomeIcon icon={faFacebook} />
                                 </span>
                             </div>
-                            <div>
-                                <span className='contact-icons text-black'>
-                                <FontAwesomeIcon icon={faPhone} />
-                                </span> 09268883090 (Smart)
-                            </div>
-
+                        </div>
+                        <div>
+                            <span className='contact-icons text-black'>
+                            <FontAwesomeIcon icon={faPhone} />
+                            </span> 09268883090 (Smart)
                         </div>
                     </Col>
 
                     <Col lg={6}>
-                        <h2 className='display--2 mb-2 text-white text-center'>Cap&aposs Foodpark Map</h2>
-                        <pre className='text-white text-center'>Location: 
+                        <h2 className='display--2 mb-2 text-white text-center'>Cap's Foodpark Map</h2>
+                        <p className='text-white text-center'>Location: 
                         #7 Katihan Poblacion, Muntinlupa City @ <br />Caps Foodpark & Garden
-                        (Near southernside montrssori)</pre>
+                        (Near southernside montrssori)</p>
                         <div className='contact-map d-flex justify-content-center'>
                             <iframe
                                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3864.691265109292!2d121.03405875757322!3d14.387266189140194!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397d17ddfee476b%3A0xe60d92e370b991b1!2sCap%27s%20Foodpark!5e0!3m2!1sen!2sph!4v1729582943445!5m2!1sen!2sph"
